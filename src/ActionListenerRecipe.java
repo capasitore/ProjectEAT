c25674686468b42c7ff4e6ed3cdf3952f9fbf6ab
capasitore <capasitore@users.noreply.github.com>
2014-11-04 15:14:10 +0100

    Create ActionListenerRecipe.java

---
+++ src/ActionListenerRecipe.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ActionListenerRecipe implements ActionListener {
+
+	
+	
+	
+	public void actionPerformed(ActionEvent search) {
+		// TODO Auto-generated method stub
+//        System.out.println("Clicked Search");
+
+		
+	}
+
+}
