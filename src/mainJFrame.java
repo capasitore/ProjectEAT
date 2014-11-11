f5c417b31bf328589beb0117ac5e9c308238f8e4
capasitore <capasitore@users.noreply.github.com>
2014-11-11 11:17:36 +0100

    split mainframe into two files
    recipelist.java for search results table

--- src/mainJFrame.java
+++ src/mainJFrame.java
@@ -1,6 +1,6 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -21,23 +21,16 @@ import javax.swing.JComboBox;
 import java.awt.Panel;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.net.URL;
-import java.util.ArrayList;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 import DB.sqliteConnection;
-import Model.Recipe;
-import Repositories.RecipeRepository;
-
 
 public class mainJFrame extends JFrame {
 
-	private JPanel contentPane;
+	protected JPanel contentPane;
 	private JTextField txtSearch;
 	private JTable table;
 
@@ -67,12 +60,13 @@ public class mainJFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{440, 0};
-		gbl_contentPane.rowHeights = new int[]{47, 47, 89, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 440, 0 };
+		gbl_contentPane.rowHeights = new int[] { 47, 47, 89, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 1.0,
+				Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		Panel panel_1 = new Panel();
 		panel_1.setBackground(Color.LIGHT_GRAY);
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
@@ -82,12 +76,12 @@ public class mainJFrame extends JFrame {
 		gbc_panel_1.gridy = 0;
 		contentPane.add(panel_1, gbc_panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{333, 220, 0};
-		gbl_panel_1.rowHeights = new int[] {10, 0};
-		gbl_panel_1.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWidths = new int[] { 333, 220, 0 };
+		gbl_panel_1.rowHeights = new int[] { 10, 0 };
+		gbl_panel_1.columnWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_1.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panel_1.setLayout(gbl_panel_1);
-		
+
 		txtSearch = new JTextField();
 		txtSearch.setText("Find by name or ingredient...");
 		txtSearch.setHorizontalAlignment(SwingConstants.LEFT);
@@ -99,8 +93,7 @@ public class mainJFrame extends JFrame {
 		gbc_txtSearch.gridy = 0;
 		panel_1.add(txtSearch, gbc_txtSearch);
 		txtSearch.setColumns(10);
-		
-		
+
 		JButton btnNewButton = new JButton("Search");
 		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
@@ -108,19 +101,19 @@ public class mainJFrame extends JFrame {
 		gbc_btnNewButton.gridx = 1;
 		gbc_btnNewButton.gridy = 0;
 		panel_1.add(btnNewButton, gbc_btnNewButton);
-		
-		//ActionListener Seach button
-		btnNewButton.addActionListener(new ActionListenerRecipe()
-		{
-		    public void actionPerformed(ActionEvent search)
-		    {
-		        System.out.println(this.getClass().getResource("Resource/projecteat.sqlite"));
-		        //for testing purposed
-		        URL dbFilePath = this.getClass().getResource("Resource/projecteat.sqlite");
-		        sqliteConnection.dbConnector(dbFilePath);
-		    }
+
+		// ActionListener Seach button
+		btnNewButton.addActionListener(new ActionListenerRecipe() {
+			public void actionPerformed(ActionEvent search) {
+				System.out.println(this.getClass().getResource(
+						"Resource/projecteat.sqlite"));
+				// for testing purposed
+				URL dbFilePath = this.getClass().getResource(
+						"Resource/projecteat.sqlite");
+				sqliteConnection.dbConnector(dbFilePath);
+			}
 		});
-		
+
 		Panel panel_2 = new Panel();
 		panel_2.setBackground(new Color(0, 204, 102));
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
@@ -129,117 +122,49 @@ public class mainJFrame extends JFrame {
 		gbc_panel_2.gridx = 0;
 		gbc_panel_2.gridy = 1;
 		contentPane.add(panel_2, gbc_panel_2);
-		
+
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"All", "Italian", "Greek", "Persian"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] { "All",
+				"Italian", "Greek", "Persian" }));
 		panel_2.add(comboBox);
-		
-		//ActionListener combobox filter 
-		//ActionListener Combobox filter by cousine
-		comboBox.addActionListener(new ActionListenerRecipe()
-		{
-		    public void actionPerformed(ActionEvent filtercousine)
-		    {
-		     JComboBox comboBox = (JComboBox)filtercousine.getSource();
-		     String selectedFilter = (String)comboBox.getSelectedItem();
-		     System.out.println(selectedFilter);
-		    }
+
+		// ActionListener combobox filter
+		// ActionListener Combobox filter by cousine
+		comboBox.addActionListener(new ActionListenerRecipe() {
+			public void actionPerformed(ActionEvent filtercousine) {
+				JComboBox comboBox = (JComboBox) filtercousine.getSource();
+				String selectedFilter = (String) comboBox.getSelectedItem();
+				System.out.println(selectedFilter);
+			}
 		});
-		
-		
-		
+
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"All", "<20 min", "<30 min", "<40 min"}));
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] { "All",
+				"<20 min", "<30 min", "<40 min" }));
 		panel_2.add(comboBox_1);
-		
-		//ActionListener combobox filter 
-		//ActionListener Combobox filter by time
-		comboBox_1.addActionListener(new ActionListenerRecipe()
-		{
-		    public void actionPerformed(ActionEvent filtertime)
-		    {
-		     JComboBox comboBox_1 = (JComboBox)filtertime.getSource();
-		     String selectedTimeFilter = (String)comboBox_1.getSelectedItem();
-		     System.out.println(selectedTimeFilter);
-		    }
+
+		// ActionListener combobox filter
+		// ActionListener Combobox filter by time
+		comboBox_1.addActionListener(new ActionListenerRecipe() {
+			public void actionPerformed(ActionEvent filtertime) {
+				JComboBox comboBox_1 = (JComboBox) filtertime.getSource();
+				String selectedTimeFilter = (String) comboBox_1
+						.getSelectedItem();
+				System.out.println(selectedTimeFilter);
+			}
 		});
-		
-		
-		Panel panel = new Panel();
-		panel.setBackground(new Color(153, 51, 0));
+
+		// RecipeList shows the recipe result from search
+		RecipeList recipeFrame = new RecipeList();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
 		gbc_panel.fill = GridBagConstraints.BOTH;
 		gbc_panel.gridx = 0;
 		gbc_panel.gridy = 2;
-		contentPane.add(panel, gbc_panel);
-		
-		
-	
+		gbc_panel.weighty = 1;
+		gbc_panel.weightx = 1;
 
-		
-		table = new JTable();
-		
-		table.setFillsViewportHeight(true);
-		DefaultTableModel model = new DefaultTableModel(
-				new Object[][] {
-				},
-				new String[] {
-					"Name",
-					"Cousine",
-					"Time Required"
-				}	
-			) {
-				Class[] columnTypes = new Class[] {
-					String.class,
-					String.class,
-					String.class
-				};
-				public Class getColumnClass(int columnIndex) {
-					return columnTypes[columnIndex];
-				}
-			};
-		
-		
-		table.setModel(model);
-		panel.setLayout(new BorderLayout());
-		panel.add(table, BorderLayout.CENTER);
-		panel.add(table.getTableHeader(), BorderLayout.NORTH);
+		contentPane.add(recipeFrame, gbc_panel);
 
-		//MouseListener for Table Column
-		table.getTableHeader().addMouseListener(new MouseAdapter() {
-		    @Override
-		    public void mouseClicked(MouseEvent e) {
-		        int col = table.columnAtPoint(e.getPoint());
-		        String name = table.getColumnName(col);
-		        System.out.println("Column selected "+ name);
-		    }
-		});
-		
-
-		
-		//ActionListener table
-		// Gets the row nr for the clicked row
-		
-		table.addMouseListener(new MouseAdapter() {
-			  public void mouseClicked(MouseEvent e) {
-			      JTable target = (JTable)e.getSource();
-			      int row = target.getSelectedRow();
-				  System.out.println(row);
-
-			      // if appropriate column do this...
-			    
-			  }
-			});
-		
-		RecipeRepository recipeRepository = new RecipeRepository();
-		ArrayList<Recipe> allRecipes = recipeRepository.GetAll();
-		for (Recipe recipe : allRecipes) {
-			model.addRow( recipe.getObjectForTable());
-		}
-
-		
-		
-		
 	}
 
 }
